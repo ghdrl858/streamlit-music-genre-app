b671e5c9b754b5fada3fbd74f0b5cc45aaea2994
ghdrl858 <ghdrl858@gmail.com>
2022-05-26 17:37:47 +0900

    add

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from app_home import run_home
-from app_eda import run_eda
+# from app_eda import run_eda
 from app_ml import run_ml
 
 def main() :
@@ -13,7 +13,7 @@ def main() :
     if choice == menu[0] :
         run_home()
     elif choice == menu[1] :
-        run_eda()
+        # run_eda()
         pass
     elif choice == menu[2] :
         run_ml()
