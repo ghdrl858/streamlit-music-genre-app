2a33bf1caeabe8a68a2bdd2629c49fb34c4d7d33
ghdrl858 <ghdrl858@gmail.com>
2022-05-26 12:45:11 +0900

    2022-05-26

--- app_ml.py
+++ app_ml.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import joblib
 import numpy as np
-import sklearn
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer, f1_score, accuracy_score, confusion_matrix, classification_report
+import scikitplot as skplt
 
 def run_ml():
     pass
