82374450aab729f1565e5c19a51656d6974813f0
ghdrl858 <ghdrl858@gmail.com>
2022-05-27 12:42:17 +0900

    2022-05-27

--- app_home.py
+++ app_home.py
@@ -8,7 +8,7 @@ def run_home() :
     # with col1 :
     #     st.image("image/black_piano.jpg", width = 850)
 
-    st.write('''#### º 
+    st.write('''##### º 다양한 장르의 음악이 여러 가지 상황과 환경에 미치는 영향을 알아봅시다.
 
     ''')
     video_file = open('video/Guitarist - 139.mp4', 'rb')
