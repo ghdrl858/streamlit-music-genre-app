41f3d6dac7354e7299f75bf1ef884807c4775374
ghdrl858 <ghdrl858@gmail.com>
2022-05-30 13:06:48 +0900

    2020-05-30

--- app_home.py
+++ app_home.py
@@ -11,7 +11,7 @@ def run_home() :
     st.write('''##### º 다양한 장르의 음악이 여러 가지 상황과 환경에 미치는 영향을 알아봅시다.
 
     ''')
-    video_file = open('video/Guitarist-139.mp4', 'rb')
+    video_file = open('video/guitarist-139.mp4', 'rb')
     video_bytes = video_file.read()
 
     st.video(video_bytes)
